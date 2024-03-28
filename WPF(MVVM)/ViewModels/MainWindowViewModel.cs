e84f07f633fcd0b6d544e38a8ea5ba09604a6eb8
Octopus <thsctbrkm@gmail.com>
2024-03-28 19:25:54 +0300

    added Clients

---
+++ WPF(MVVM)/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using WPF_MVVM_.Models;
+using WPF_MVVM_.ViewModels.Base;
+
+namespace WPF_MVVM_.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _title = "kukareku";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        private List<ClientModel> _clients;
+
+        public List<ClientModel> Clients
+        {
+            get => _clients;
+            set => Set(ref _clients, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            _clients = new List<ClientModel>();
+
+            using (var reader = new StreamReader("clients.csv"))
+            using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
+            {
+
+                _clients = csv.GetRecords<ClientModel>().ToList();
+            }
+        }
+    }
+}
--- WPF(MVVM)/Views/MainWindow.xaml.cs
+++ WPF(MVVM)/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_MVVM_.ViewModels;
 
 namespace WPF_MVVM_
 {
@@ -23,6 +24,7 @@ namespace WPF_MVVM_
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
